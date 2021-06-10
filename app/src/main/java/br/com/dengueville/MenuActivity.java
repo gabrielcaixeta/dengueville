9be09b57cc63481e02462487aa8191faff01af68
lucas-leoni <lucas.leoni09@gmail.com>
2021-06-09 22:16:25 -0300

    Criada a função abrir telefone

--- app/src/main/java/br/com/dengueville/MenuActivity.java
+++ app/src/main/java/br/com/dengueville/MenuActivity.java
@@ -2,7 +2,10 @@ package br.com.dengueville;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+    public void abrirTelefone(View novaView) {
+        Intent intencao = new Intent(Intent.ACTION_DIAL);
+        intencao.setData(Uri.parse("tel:04734313233"));
+        startActivity(intencao);
     }
 }
