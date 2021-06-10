b6adc6041039120cc6145a1acb88a5fc4678de74
lucas-leoni <lucas.leoni09@gmail.com>
2021-06-10 20:44:46 -0300

    Criando string para número de telefone

--- app/src/main/java/br/com/dengueville/MainActivity.java
+++ app/src/main/java/br/com/dengueville/MainActivity.java
@@ -2,7 +2,10 @@ package br.com.dengueville;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void abrirTelefone(View novaView) {
+        String numeroTelefone = getString(R.string.numero_telefone);
+        Intent intencao = new Intent(Intent.ACTION_DIAL);
+        intencao.setData(Uri.parse(numeroTelefone));
+        startActivity(intencao);
     }
 }
