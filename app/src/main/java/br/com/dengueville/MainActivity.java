e7c411bc7ef8d17e41dcf84714397abbddc0cdfb
Alexandre Salvador <62520840+alexandresalvador@users.noreply.github.com>
2021-07-13 20:53:33 -0300

    first commit

--- app/src/main/java/br/com/dengueville/MainActivity.java
+++ app/src/main/java/br/com/dengueville/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,10 +15,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public void abrirTelefone(View novaView) {
         String numeroTelefone = getString(R.string.numero_telefone);
         Intent intencao = new Intent(Intent.ACTION_DIAL);
         intencao.setData(Uri.parse(numeroTelefone));
+        startActivity(intencao);
+    }
+    public void abrirTelaSintoma(View novaView) {
+        Intent intencao = new Intent(this,SintomasActivity.class);
+        startActivity(intencao);
+    }
+
+    public void voltarTelaMain(View novaView) {
+        Intent intencao = new Intent(this,MainActivity.class);
         startActivity(intencao);
     }
 }
